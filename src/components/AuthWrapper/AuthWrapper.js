07effe67aa84c00e2dd90591bb85b1411afed171
Michael Pattison <mpattison80@gmail.com>
2021-07-09 16:50:08 +0200

    Update AuthWrapper.js

--- src/components/AuthWrapper/AuthWrapper.js
+++ src/components/AuthWrapper/AuthWrapper.js
@@ -4,8 +4,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 export const AuthWrapper = ({ children }) => {
   const curOrganization = localStorage.getItem("organization_id");
   const options = {
-    domain: "dev-rm3u1yq4.auth0.com",
-    clientId: "OgdvG5snvJGpeIrW9FKXbB5D9UmSQufP",
+    domain: "training-mp-dev.eu.auth0.com",
+    clientId: "fu0KIw5QQMp1bo3hP161qYyUWkEHQhQs",
     redirectUri: window.location.origin,
     ...(curOrganization ? { organization: curOrganization } : null)
   };
